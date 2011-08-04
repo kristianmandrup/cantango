ee9a5de09a2be7ddafa02cf7ea20772400f14db1
Kristian Mandrup <kmandrup@gmail.com>
2011-08-04 22:44:43 +0200

    configuring guest_user in cantango initializer

--- spec/devise-dummy/config/initializers/cantango.rb
+++ spec/devise-dummy/config/initializers/cantango.rb
@@ -2,6 +2,7 @@ CanTango.configure do |config|
   config.guest_user Proc.new { Guest.instance }
 end
 
-puts "guest_user_procedure:"
-puts CanTango::Configuration.guest_user_procedure
+# puts "guest_user_procedure:"
+# puts CanTango::Configuration.methods.sort
+# puts CanTango::Configuration.guest_user_procedure.inspect
 
