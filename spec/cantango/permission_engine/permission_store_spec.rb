ee50ba213f0bad0cd56f9208f6b78c1130a1ca54
stanislaw <s.pankevich@gmail.com>
2011-08-09 20:22:09 +0300

    added permission_engine into entire_suite

--- spec/cantango/permission_engine/permission_store_spec.rb
+++ spec/cantango/permission_engine/permission_store_spec.rb
@@ -3,7 +3,6 @@ require 'cantango'
 
 describe 'Permission store' do
   let (:permission_store) { CanTango.permission_store}
-    its(:cache) { should_not be_nil}
 end
 
 
--- spec/cantango/permission_engine/user_permission_store_spec.rb
+++ spec/cantango/permission_engine/user_permission_store_spec.rb
@@ -3,7 +3,6 @@ require 'cantango'
 
 describe 'User permission store' do
   let (:user_permission_store) { CanTango.user_permission_store}
-    its(:cache) { should_not be_nil}
 end
 
 
