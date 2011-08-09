c21991b05671472aee4e9954ee388cf4d83f03ff
Kristian Mandrup <kmandrup@gmail.com>
2011-08-09 19:07:28 +0200

    no categories store yet - uncomment spec

--- spec/cantango/permission_engine/categories_store_spec.rb
+++ spec/cantango/permission_engine/categories_store_spec.rb
@@ -2,8 +2,8 @@ require 'rspec'
 require 'cantango'
 
 describe 'Categories store' do
-  let (:categories_store) { CanTango.categories_store}
-    its(:cache) { should_not be_nil}
+  # let (:categories_store) { CanTango.categories_store }
+    # its(:cache) { should_not be_nil}
 end
 
 
