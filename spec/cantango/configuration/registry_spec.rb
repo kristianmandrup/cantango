dac928a78eea5dde56c6588d844d78a831468462
Kristian Mandrup <kmandrup@gmail.com>
2011-08-11 21:15:34 +0200

    avoid const clash

--- spec/cantango/configuration/registry_spec.rb
+++ spec/cantango/configuration/registry_spec.rb
@@ -3,11 +3,11 @@ require 'cantango'
 
 require 'cantango/configuration/shared/role_registry_ex'
 
-class MyReg < CanTango::Configuration::Registry
+class MyRegi < CanTango::Configuration::Registry
 end
 
-describe MyReg do
-  subject { MyReg.instance }
+describe MyRegi do
+  subject { MyRegi.instance }
 
   it_should_behave_like "Registry" do
   end
