8f5152f9a3c220b2e00585b897d14b704c44023e
Kristian Mandrup <kmandrup@gmail.com>
2011-08-16 21:28:54 +0200

    fixed remaining routing bugs in dummy apps

--- spec/devise-dummy/app/helpers/application_helper.rb
+++ spec/devise-dummy/app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def delete_user_path user
+    user_path(user), :method => 'delete'
+  end
+
+  def delete_post_path post
+    post_path(post), :method => 'delete'
+  end
 end
