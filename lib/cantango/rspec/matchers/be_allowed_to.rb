6ef46a6fb2fa79959ad6742af8f94ca0124aa2f1
Kristian Mandrup <kmandrup@gmail.com>
2011-08-25 00:37:11 +0200

    bumped version and release notes

--- lib/cantango/rspec/matchers/be_allowed_to.rb
+++ lib/cantango/rspec/matchers/be_allowed_to.rb
@@ -5,17 +5,19 @@ RSpec::Matchers.define :be_allowed_to do |action, objects|
   chain :with_options do |options|
     @options = options
   end
-  
+
   match do |subject|
     @options ? subject.can?(action, objects, @options) : subject.can?(action, objects)
   end
 
   failure_message_for_should do |subject|
-    "expected that #{subject} could a #{action} the #{objects}"
+    %{expected that #{subject} could #{action} the #{objects}
+#{subject.send :rules}}
   end
 
   failure_message_for_should_not do |actual|
-    "did not expect that #{subject} could a #{action} the #{objects}"
+    %{did not expect that #{subject} could #{action} the #{objects}
+#{subject.send :rules}}
   end
 
   description do
