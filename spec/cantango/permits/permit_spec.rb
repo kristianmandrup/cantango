0c5ee19c0d7c610bff36619b68471f46871a8e0c
stanislaw <s.pankevich@gmail.com>
2011-09-01 19:32:41 +0300

    added double sub-classing specs (specially for Kris)

--- spec/cantango/permits/permit_spec.rb
+++ spec/cantango/permits/permit_spec.rb
@@ -12,11 +12,35 @@ class AdminPermit < CanTango::UserPermit; end
 
 describe CanTango::Permits::Permit do
 
-  it "should register various permits" do
+  it "should register RolePermit-based permits" do
     CanTango.config.permits.admin_account.role[:admin].should == AdminAccountPermits::AdminRolePermit
     CanTango.config.permits.role[:musician].should == MusicianRolePermit
+  end
+  
+  it "should register RoleGroupPermit-based permits" do
     CanTango.config.permits.role_group[:editors].should == EditorsRoleGroupPermit
-    CanTango.config.permits.account[:admin].should == AdminAccountPermit
+  end
+ 
+  it "should register UserPermit-based permits" do
     CanTango.config.permits.user[:admin].should == AdminPermit
+  end
+  it "should register AccountPermit-based permits" do
+    CanTango.config.permits.account[:admin].should == AdminAccountPermit
+  end
+
+  context "registration of double-inherited permits (fx RolePermit ones) - specially for Kris ;)" do
+    before do
+      class GuitaristRolePermit < MusicianRolePermit; end
+      class AdminAccountPermits::AngryAdminRolePermit < AdminAccountPermits::AdminRolePermit; end
+    end
+
+    it "should register basic namespaced" do
+      CanTango.config.permits.role[:guitarist].should == GuitaristRolePermit
+    end
+
+    it "should register account-namespaced" do
+      CanTango.config.permits.admin_account.role[:angry_admin].should == AdminAccountPermits::AngryAdminRolePermit
+    end
+
   end
 end
