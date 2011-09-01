b2e7bb6cbddeed519afdc705d733a464e13e4d53
stanislaw <s.pankevich@gmail.com>
2011-09-01 23:57:06 +0300

    supplied spec for raising on non-valid actions

--- spec/cantango/permission_engine/compiler_spec.rb
+++ spec/cantango/permission_engine/compiler_spec.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 require 'cantango'
 require 'hashie'
+require 'spec_helper'
 def permission_fixture
   group = 'bloggers'
   rules = Hashie::Mash.new({"can"=>{"read"=>["Article", "Comment"]}, "cannot"=>{"write"=>["Article", "Post"]}})
@@ -15,6 +16,13 @@ describe CanTango::PermissionEngine::Compiler do
   let(:compiler) do 
     compiler = CanTango::PermissionEngine::Compiler.new
     compiler.compile! permission
+  end
+
+  it 'should raise if permission contains not-valid actions' do
+    expect {compiler.can_eval}.not_to raise_error
+    # Replacing static_rules with rule that has not-valid action:
+    permission.static_rules.can = Hashie::Mash.new({'edit' => ["Article"]})
+    expect {compiler.can_eval}.to raise_error
   end
 
   it 'should produce cancan statements' do
