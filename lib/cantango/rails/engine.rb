5165faf8ae5b3244f104efaf25f45ec6264f1423
stanislaw <s.pankevich@gmail.com>
2011-10-14 03:56:53 +0300

    reload permits on each request in development mode using config.to_prepare

--- lib/cantango/rails/engine.rb
+++ lib/cantango/rails/engine.rb
@@ -25,8 +25,10 @@ module CanTango
 
       # load all models
       # this is needed in order to register all users and accounts with CanTango using the user/account macros!
+    end
 
-      # load all permits
+    config.to_prepare do
+      # load all permits (development mode: EVERY request!)
       RailsAutoLoader.load_permits! if CanTango.config.autoload.permits?
     end
   end
