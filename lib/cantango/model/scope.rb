36ac5ae2d8367084e72d2e733f974971ac214bf2
Kristian Mandrup <kmandrup@gmail.com>
2011-10-30 15:02:43 +0100

    added negative tests to scope apis

--- lib/cantango/model/scope.rb
+++ lib/cantango/model/scope.rb
@@ -14,10 +14,11 @@ module CanTango::Model
       include CanTango::Api::User::Ability
 
 
-      attr_reader :actions, :clazz
+      attr_reader :actions, :mode, :clazz
 
-      def initialize clazz, *actions
+      def initialize clazz, mode, *actions
         @clazz = clazz
+        @mode = mode
         @actions = actions.flatten
       end
 
@@ -35,21 +36,33 @@ module CanTango::Model
       def check ability
         clazz.all.select do |obj|
           actions.all? do |action|
-            ability.can? action.to_sym, obj
+            ability.send mode_action, action.to_sym, obj
           end
         end
+      end
+
+      def mode_action
+        "#{mode}?"
       end
     end
 
     module ClassMethods
       def allowed_to *actions
-        CanTango::Model::Scope::AllowedActions.new self, *actions
+        CanTango::Model::Scope::AllowedActions.new self, :can, *actions
+      end
+
+      def not_allowed_to *actions
+        CanTango::Model::Scope::AllowedActions.new self, :cannot, *actions
       end
 
       CanTango::Model::Scope.rest_actions.each do |action|
         meth_name = action.to_s.sub(/e$/, '') << "able"
         define_method :"#{meth_name}_by" do |user|
           all.select {|obj| obj.user_ability(user).can? action.to_sym, obj }
+        end
+
+        define_method :"not_#{meth_name}_by" do |user|
+          all.select {|obj| obj.user_ability(user).cannot? action.to_sym, obj }
         end
       end
     end
