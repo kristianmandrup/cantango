bae91b3ae1cf264a4aa0d73f76934b2d0cf7dc79
Kristian Mandrup <kmandrup@gmail.com>
2011-10-31 11:51:46 +0100

    very initial idea for permit engine cache

--- lib/cantango/permit_engine.rb
+++ lib/cantango/permit_engine.rb
@@ -3,7 +3,18 @@ module CanTango
     autoload_modules :Builder, :Compatibility, :Executor
     autoload_modules :Factory, :Finder, :Loaders, :Util, :RoleMatcher
 
-    delegate :rules, :to => :ability
+    include CanTango::Ability::CacheHelpers
+    include CanTango::Helpers::RoleMethods
+
+    # delegate :rules, :to => :ability
+
+    def rules
+      @rules ||= []
+    end
+
+    def clear_rules!
+      @rules ||= []
+    end
 
     def initialize ability
       super
@@ -12,16 +23,29 @@ module CanTango
     def execute!
       return if !valid?
       debug "Permit Engine executing..."
+      return cached_rules if cached_rules?
 
+      clear_rules!
+      permit_rules
+
+      cache_rules!
+      rules
+    end
+
+    def permit_rules
       # push result of each permit type execution into main ability rules array
       permits.each_pair do |type, permits|
         permit_rules = executor(type, permits).execute!
         rules << permit_rules if !permit_rules.blank?
       end
-   end
+    end
 
     def executor type, permits
       CanTango::Permits::Executor.new ability, type, permits
+    end
+
+    def cache
+      @cache ||= CanTango::Ability::Cache.new self, :cache_key => cache_key, :key_method_names => key_method_names
     end
 
     def engine_name
@@ -53,6 +77,25 @@ module CanTango
 
     def permit_factory
       @permit_factory ||= CanTango::PermitEngine::Factory.new ability
+    end
+
+    def cache_key
+      :permits
+    end
+
+    def key_method_names
+      permits.keys.map {|type| key type }.compact
+    end
+
+    def key type
+      case type
+      when :role
+        roles_list_meth
+      when :role_group
+        role_groups_list_meth
+      else
+        nil
+      end
     end
   end
 end
--- spec/cantango/permit_engine_spec.rb
+++ spec/cantango/permit_engine_spec.rb
@@ -1,2 +1,35 @@
-# its(:permit_class_names) { should include("UserRolePermit", "AdminsRoleGroupPermit") }
+require 'rspec'
+require 'cantango'
+require 'fixtures/models'
+require 'cantango/rspec'
+
+def config_folder
+  File.dirname(__FILE__)+ "/../fixtures/config/"
+end
+
+CanTango.configure do |config|
+  config.clear!
+end
+
+describe CanTango::PermitEngine do
+  before do
+    @user = User.new 'kris'
+  end
+
+  describe 'UserAc engine' do
+    let (:ability) do
+      CanTango::Ability.new @user
+    end
+    subject { CanTango::PermitEngine.new ability }
+
+    describe '#execute!' do
+      before do
+        subject.execute!
+      end
+
+      specify { subject.ability.send(:rules).should_not be_empty }
+    end
+  end
+end
+
 
