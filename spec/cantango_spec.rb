847dee193d389086d434c72f988b39b8f9cd35da
Kristian Mandrup <kristian@unity3d.com>
2011-11-17 15:55:23 +0100

    cantango spec

--- spec/cantango_spec.rb
+++ spec/cantango_spec.rb
@@ -7,7 +7,25 @@ describe CanTango do
     specify { lambda { CanTango::Cache::MonetaCache }.should raise_error }
     specify { lambda { CanTango::Ability::Cache::MonetaCache }.should raise_error }
     specify { lambda { CanTango::PermissionEngine::MonetaStore }.should raise_error }
+    
+    describe 'permits allowed/denied' do
+      specify { lambda { subject.permits_allowed }.should raise_error }
+      specify { lambda { subject.permits_denied }.should raise_error }
+    end
+
+    [:config, :configure].each do |name|
+      specify { subject.send(name).should be_a CanTango::Configuration }
+    end
+    
+    describe 'clear_permits_executed!' do
+      specify { CanTango.config.permits.executed.should be_empty }
+      
+      before do
+        CanTango.config.permits.executed[:x] = 1
+        subject.clear_permits_executed!
+      end
+      
+      specify { CanTango.config.permits.executed.should be_empty }
+    end
   end
 end
-
-
